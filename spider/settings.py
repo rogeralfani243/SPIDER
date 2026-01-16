66defa8c1a148733f86f5e76de66fc6cf11a0232
winalfani243 <winalfani@gmail.com>
2026-01-16 16:44:13 -0700

    initial commit

--- spider/settings.py
+++ spider/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bnt3@*u*vwxxv6aue%*bbs6w+%r#e6!77gl$24$kg$^4*-%f@3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
