6a6aa782f4e60a454dd8167e4f1e41d64bdc6564
winalfani243 <winalfani@gmail.com>
2026-01-16 19:49:18 -0700

    sixth commit 2

--- client/src/hooks/useApiUrl.js
+++ client/src/hooks/useApiUrl.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://spider-app-d4d82ba4f1c1.herokuapp.com/';
 
 
 export default API_URL;
