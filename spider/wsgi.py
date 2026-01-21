fd9494a0a03fa4a04b850dfa42624d334999d892
winalfani243 <winalfani@gmail.com>
2026-01-21 00:36:18 -0700

    commit no.11

--- spider/wsgi.py
+++ spider/wsgi.py
@@ -9,9 +9,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spider.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='staticfiles')
