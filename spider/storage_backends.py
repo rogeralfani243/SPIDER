8a6b5e74e946d28c9fb38c808b0d6ef04e655b18
winalfani243 <winalfani@gmail.com>
2026-01-23 15:49:58 -0700

    commit np.43

--- spider/storage_backends.py
+++ spider/storage_backends.py
@@ -1,15 +1,20 @@
-# myapp/storage_backends.py
+
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
+    file_overwrite = True
 
-class MediaStorage(S3Boto3Storage):
+class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
-    default_acl = 'public-read'
+    default_acl = 'public-read'  # ← ESSENTIEL
     file_overwrite = False
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
 
-# Puis dans settings.py:
-# DEFAULT_FILE_STORAGE = 'myapp.storage_backends.MediaStorage'
-# STATICFILES_STORAGE = 'myapp.storage_backends.StaticStorage'
+class PrivateMediaStorage(S3Boto3Storage):
+    location = 'private'
+    default_acl = 'private'
+    file_overwrite = False
+    custom_domain = False
