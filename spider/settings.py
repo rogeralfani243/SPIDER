72c2b634423c89dd56a9cde174971e21a13183a6
winalfani243 <winalfani@gmail.com>
2026-01-23 14:52:01 -0700

    commit np.34

--- spider/settings.py
+++ spider/settings.py
@@ -241,28 +241,53 @@ TEMPLATES = [
             ],
         },
     },
-]
-if not DEBUG:  # Production
-    # ===== MEDIA =====
+]# Détection d'environnement Heroku
+IS_HEROKU = "DYNO" in os.environ
+
+# Configuration S3 pour la production
+if IS_HEROKU or not DEBUG:  # Production Heroku ou autre
+    # ===== CONFIGURATION AWS S3 =====
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-west-3')  # Paris par défaut
     
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-
+    # Configuration S3
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_LOCATION = 'media'
     AWS_QUERYSTRING_AUTH = False
-    AWS_DEFAULT_ACL = None
-    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'public-read'  # ← CHANGEMENT IMPORTANT
     AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-    # ===== STATIC =====
+    AWS_S3_ADDRESSING_STYLE = "virtual"
+    
+    # ===== MEDIA FILES (Uploads utilisateurs) =====
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    
+    # ===== STATIC FILES (CSS, JS) =====
+    # Sur Heroku, utilisez Whitenoise pour les static files
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+    
+    # Optimisation Whitenoise
+    WHITENOISE_USE_FINDERS = True
+    WHITENOISE_MANIFEST_STRICT = False
+    WHITENOISE_ALLOW_ALL_ORIGINS = True
+    
+else:  # Développement local
+    # ===== LOCAL DEVELOPMENT =====
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+    
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
