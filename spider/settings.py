113047042771a2c42ed482ff91565997675322dc
winalfani243 <winalfani@gmail.com>
2026-01-23 17:24:52 -0700

    commit np.47

--- spider/settings.py
+++ spider/settings.py
@@ -279,7 +279,12 @@ AWS_LOCATION = "media"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
 AWS_QUERYSTRING_AUTH = False
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
+# Disable ACLs
+AWS_DEFAULT_ACL = None 
 
 if IS_HEROKU:
     # === Production ===
