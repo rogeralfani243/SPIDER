73818013eb95b0d0267cad5bc839630e1000203d
winalfani243 <winalfani@gmail.com>
2026-01-23 17:56:37 -0700

    commit no.49

--- spider/settings.py
+++ spider/settings.py
@@ -275,14 +275,14 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME','amz-spider-app') 
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME','eu-north-1')            # eu-north-1
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 AWS_LOCATION = "media"
-
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 # Disable ACLs
 AWS_DEFAULT_ACL = None 
 MEDIA_ROOT = BASE_DIR / "media"
