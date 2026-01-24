0e05b3e6fc58d08b76a2326ceb8caf2e47f8eed3
winalfani243 <winalfani@gmail.com>
2026-01-23 17:32:22 -0700

    commit no.48

--- spider/storage_backends.py
+++ spider/storage_backends.py
@@ -1,20 +1,19 @@
-
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = 'public-read'
+    default_acl = None  # Changed from 'public-read'
     file_overwrite = True
 
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
-    default_acl = 'public-read'  # ← ESSENTIEL
+    default_acl = None  # Changed from 'public-read' ← THIS IS CRITICAL
     file_overwrite = False
     custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
 
 class PrivateMediaStorage(S3Boto3Storage):
     location = 'private'
-    default_acl = 'private'
+    default_acl = None  # Changed from 'private'
     file_overwrite = False
     custom_domain = False
