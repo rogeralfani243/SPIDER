8d82a2f0b0eace7426355609949e8637d0b489fe
winalfani243 <winalfani@gmail.com>
2026-01-23 18:59:16 -0700

    commit no.50

--- spider/settings.py
+++ spider/settings.py
@@ -275,6 +275,13 @@ WHITENOISE_ALLOW_ALL_ORIGINS = True
 # ===== S3 MEDIA STORAGE (FORCED) =====
 # === DÉTECTION ENVIRONNEMENT ===
 IS_HEROKU = "DYNO" in os.environ
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "eu-north-1")
+AWS_QUERYSTRING_AUTH = False  # pour que les fichiers publics soient accessibles
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
 
 # === CONFIGURATION STORAGE ===
 # TOUJOURS utiliser S3 sur Heroku, même en DEBUG
