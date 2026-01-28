f12e99f0e6d1bad50fe744b57c48ae21f552b197
winalfani243 <winalfani@gmail.com>
2026-01-27 20:28:04 -0700

    commit no.61

--- spider/settings.py
+++ spider/settings.py
@@ -252,30 +252,24 @@ TEMPLATES = [
 # settings.py - VERSION CORRIGÉE
 
 
-# S3 Configuration
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "eu-north-1")
 
 if AWS_STORAGE_BUCKET_NAME:
-    # Forcer S3 pour STATIC et MEDIA
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-    AWS_QUERYSTRING_AUTH = False  # URLs publiques sans signature
-    AWS_DEFAULT_ACL = None        # Evite les ACL conflictuelles
+    AWS_QUERYSTRING_AUTH = False
+    AWS_DEFAULT_ACL = None
 
-    # Domaine S3
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-    # URLs publiques
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
-# IMPORTANT pour collectstatic
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
