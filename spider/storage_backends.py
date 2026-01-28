c57a8223f6930d0cb9050a2d211aedc194088719
winalfani243 <winalfani@gmail.com>
2026-01-28 13:53:32 -0700

    commit no.76

--- spider/storage_backends.py
+++ spider/storage_backends.py
@@ -13,7 +13,7 @@ class MediaStorage(S3Boto3Storage):  # ← CHANGÉ ICI
     default_acl = None
     file_overwrite = False
     custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
-
+    object_parameters = {}
 class PrivateMediaStorage(S3Boto3Storage):
     location = 'private'
     default_acl = None
