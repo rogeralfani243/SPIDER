17ee88da053e33bceb876fecd48006d93368b8ec
winalfani243 <winalfani@gmail.com>
2026-01-27 23:55:06 -0700

    commit no.68

--- spider/settings.py
+++ spider/settings.py
@@ -14,7 +14,15 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# TOUT EN HAUT de settings.py
+import os
 
+# Override par variables d'environnement
+if os.environ.get('DJANGO_DEFAULT_FILE_STORAGE'):
+    DEFAULT_FILE_STORAGE = os.environ['DJANGO_DEFAULT_FILE_STORAGE']
+    STATICFILES_STORAGE = os.environ['DJANGO_STATICFILES_STORAGE']
+    STATIC_URL = os.environ.get('DJANGO_STATIC_URL', '/static/')
+    MEDIA_URL = os.environ.get('DJANGO_MEDIA_URL', '/media/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
