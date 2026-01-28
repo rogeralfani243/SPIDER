ec5f3e1be3e90712915ccb39b97dcf0b9ad6419e
winalfani243 <winalfani@gmail.com>
2026-01-27 19:52:28 -0700

    commit no.59

--- spider/settings.py
+++ spider/settings.py
@@ -262,7 +262,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "eu-west-3")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 if AWS_STORAGE_BUCKET_NAME:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
