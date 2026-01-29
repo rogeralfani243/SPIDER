38750535f1b45b045eda449d37e071b00a571a77
winalfani243 <winalfani@gmail.com>
2026-01-28 20:00:11 -0700

    commit no.78

--- spider/settings.py
+++ spider/settings.py
@@ -176,7 +176,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "eu-north-1")
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-
+DEFAULT_FILE_STORAGE = 'spider.storage_backends.MediaStorage'
+    
 # Si sur Heroku ET S3 configuré
 if IS_HEROKU and AWS_STORAGE_BUCKET_NAME and AWS_ACCESS_KEY_ID:
     print(f"🚀 Activation S3 pour Heroku - Bucket: {AWS_STORAGE_BUCKET_NAME}")
