75ee8de7d82fed354a4872d9ba6d1b8812c31dcd
winalfani243 <winalfani@gmail.com>
2026-01-29 14:10:34 -0700

    commit no.84

--- spider/settings.py
+++ spider/settings.py
@@ -252,7 +252,7 @@ else:
 
 """        
 # REST Framework
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
