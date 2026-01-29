f0eb09f81d933dcfc43b927afa415febb24bff3e
winalfani243 <winalfani@gmail.com>
2026-01-29 13:44:57 -0700

    commit no.84

--- spider/settings.py
+++ spider/settings.py
@@ -185,7 +185,7 @@ AWS_S3_VERIFY = True
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
