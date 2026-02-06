d7d0a79094f049be151fffed21001a3da1bd04f0
winalfani243 <winalfani@gmail.com>
2026-02-05 18:50:47 -0700

    commit 034

--- client/src/utils/useDrag.js
+++ client/src/utils/useDrag.js
@@ -2,7 +2,10 @@ const useParamDrag = () => {
   const prevent = (e) => e.preventDefault();
 
   return {
-
+    onSelectStart: prevent,
+    onContextMenu: prevent,
+    onCopy: prevent,
+    onDragStart: prevent,
    
   };
 };
